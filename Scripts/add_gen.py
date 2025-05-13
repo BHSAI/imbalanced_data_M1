df9511cd4fdb27f3e37d4798bef2c9d0557dd806
Souvik Dey <sdey@bhsai.org>
2025-05-13 15:10:56 -0400

    Updated scripts

--- Scripts/add_gen.py
+++ Scripts/add_gen.py
@@ -1,126 +1,96 @@
+import logging
+
 import numpy as np
 import pandas as pd
-from rdkit import Chem
-from rdkit.Chem import AllChem
 from rdkit.Chem import PandasTools
-from rdkit import DataStructs
-from chembl_structure_pipeline import standardizer
-from chembl_structure_pipeline import exclude_flag
 
-def is_valid_structure(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    if mol is None:
-        return False
-    exclude = exclude_flag.exclude_flag(mol)
-    return not exclude
+from utils import is_valid_structure, standardize_and_canonicalize, computeMorganFP
 
-def standardize_and_canonicalize(smiles):
-    try:
-        molecule = Chem.MolFromSmiles(smiles)
-        if molecule is None:
-            return None
-        m_no_salts = standardizer.get_parent_mol(molecule) # Remove salts
-        tostandarize = m_no_salts[0]
-        std_mol = standardizer.standardize_mol(tostandarize)
-        canonical_smiles = Chem.MolToSmiles(std_mol)
-        return canonical_smiles
-    except Exception as e:
-        print(f"Error processing SMILES: {smiles}")
-        print(e)
-        return None
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    
+def main():
+    '''
+    1. Load the original and RNN/DEG generated datasets
+    2. Combine them with labels, standardize the SMILES and remove duplicates
+    '''
 
-def computeMorganFP(mol, depth=2, nBits=1024):
-    a = np.zeros(nBits)
-    try:
-      DataStructs.ConvertToNumpyArray(AllChem.GetMorganFingerprintAsBitVect(mol,depth,nBits),a)
-    except:
-      return None
-    return a
+    training_set = pd.read_csv('../Data/Training/M1/M1_training_set.csv', index_col=None)
+    gen_inactives = pd.read_csv('../Data/Training/M1/M1_RNN_inactives.csv', index_col=None)
+    gen_inactives[['Activity']]=2
 
-'''
-1. Load the original and RNN/DEG generated datasets
-2. Combine them with labels, standardize the SMILES and remove duplicates
-'''
+    all_data = pd.concat([training_set, gen_inactives]).reset_index(drop=True)
 
-training_set = pd.read_csv('../Data/Training/M1_training_set.csv', index_col=None)
+    all_data['is_valid'] = all_data['SMILES'].apply(is_valid_structure)
+    excluded_df = all_data[all_data['is_valid'] == False].drop(columns=['is_valid'])
+    df_valid = all_data[all_data['is_valid']== True].drop(columns=['is_valid'])
+    df_valid['SMILES'] = df_valid['SMILES'].apply(standardize_and_canonicalize)
 
-gen_inactives = pd.read_csv('../Data/Input/M1_RNN_inactives.csv', index_col=0)
-gen_inactives.columns = ['SMILES']
-gen_inactives[['Activity']]=2
+    '''
+    3. Arrange the SMILES based on length
+    4. Remove all RNN/DEG generated SMILES that are shorter than the smallest original SMILES and remove duplicates
+    '''
 
-all_data_filter = pd.concat([training_set, gen_inactives]).reset_index(drop=True)
+    dfx = df_valid
+    all_rdkit_noduplicates = dfx.drop_duplicates(subset=['SMILES','Activity']).dropna().sort_values(by="SMILES", key=lambda x: x.str.len()).reset_index(drop=True)
 
-all_data_filter['is_valid'] = all_data_filter['SMILES'].apply(is_valid_structure)
-excluded_df = all_data_filter[all_data_filter['is_valid'] == False].drop(columns=['is_valid'])
-df_valid = all_data_filter[all_data_filter['is_valid']== True].drop(columns=['is_valid'])
-df_valid['SMILES'] = df_valid['SMILES'].apply(standardize_and_canonicalize)
+    min_index = next(i for i, val in enumerate(dfx['Activity']) if val == 0)
+    all_rdkit_noduplicates = dfx.iloc[min_index:].sort_values(by="Activity").drop_duplicates(subset=['SMILES']).reset_index(drop=True)
 
-'''
-3. Arrange the SMILES based on length
-4. Remove all RNN/DEG generated SMILES that are shorter than the smallest original SMILES and remove duplicates
-'''
+    print('Gen Combined', all_rdkit_noduplicates['Activity'].value_counts())
+    all_rdkit_noduplicates = all_rdkit_noduplicates.drop_duplicates(subset=['SMILES'])
+    print('Gen Combined w/o duplicates', all_rdkit_noduplicates['Activity'].value_counts())
 
-dfx = df_valid
-all_rdkit_noduplicates = dfx.drop_duplicates(subset=['SMILES','Activity']).dropna().sort_values(by="SMILES", key=lambda x: x.str.len()).reset_index(drop=True)
+    '''
+    5. Remove duplicates after sorting actives, inactives and generated compounds
+    '''
 
-for i in range(all_rdkit_noduplicates.shape[0]):
-    if all_rdkit_noduplicates['Activity'][i]==0:
-        print(i)
-        break
+    PandasTools.AddMoleculeColumnToFrame(frame=all_rdkit_noduplicates, smilesCol='SMILES', molCol='Molecule')
+    all_rdkit_noduplicates['Morgan2FP'] = all_rdkit_noduplicates['Molecule'].map(computeMorganFP)
 
-all_rdkit_noduplicates = all_rdkit_noduplicates.iloc[i:].sort_values(by="Activity").reset_index(drop=True)
-print('Gen Combined', all_rdkit_noduplicates['Activity'].value_counts())
-all_rdkit_noduplicates = all_rdkit_noduplicates.drop_duplicates(subset=['SMILES'])
-print('Gen Combined w/o duplicates', all_rdkit_noduplicates['Activity'].value_counts())
+    gen_training = all_rdkit_noduplicates[all_rdkit_noduplicates['Activity']==2].reset_index(drop=True)
+    actives = all_rdkit_noduplicates[all_rdkit_noduplicates['Activity']==1].reset_index(drop=True)
+    inactives = all_rdkit_noduplicates[all_rdkit_noduplicates['Activity']==0].reset_index(drop=True)
 
-'''
-5. Remove duplicates after sorting actives, inactives and generated compounds
-'''
+    source_ac = []
+    source_in = []
 
-PandasTools.AddMoleculeColumnToFrame(frame=all_rdkit_noduplicates, smilesCol='SMILES', molCol='Molecule')
-all_rdkit_noduplicates['Morgan2FP'] = all_rdkit_noduplicates['Molecule'].map(computeMorganFP)
-gen_training = all_rdkit_noduplicates[all_rdkit_noduplicates['Activity']==2].reset_index(drop=True)
-ac = all_rdkit_noduplicates[all_rdkit_noduplicates['Activity']==1].reset_index(drop=True)
-inac = all_rdkit_noduplicates[all_rdkit_noduplicates['Activity']==0].reset_index(drop=True)
+    for i in range(inactives.shape[0]):
+        for j in range(gen_training.shape[0]):
+            if np.array_equal(inactives.Morgan2FP[i],gen_training.Morgan2FP[j]):
+                print('Inactives overlap',i,j)
+                source_in.append(j)
 
-source_ac = []
-source_in = []
+    for i in range(actives.shape[0]):
+        for j in range(gen_training.shape[0]):
+            if np.array_equal(actives.Morgan2FP[i],gen_training.Morgan2FP[j]):
+                print('Actives overlap',i,j)
+                source_ac.append(j)
 
-for i in range(inac.shape[0]):
-    for j in range(gen_training.shape[0]):
-        if np.array_equal(inac.Morgan2FP[i],gen_training.Morgan2FP[j]):
-            print('Inactives',i,j)
-            source_in.append(j)
+    gen_training = gen_training.drop(source_in + source_ac).reset_index(drop=True)
 
-for i in range(ac.shape[0]):
-    for j in range(gen_training.shape[0]):
-        if np.array_equal(ac.Morgan2FP[i],gen_training.Morgan2FP[j]):
-            print('Actives',i,j)
-            source_ac.append(j)
+    '''
+    5. Combine actives, inactives and RNN/DEG, and remove duplicates with external test set
+    '''
 
-gen_training = gen_training.drop(source_in).reset_index(drop=True)
-gen_training = gen_training.drop(source_ac).reset_index(drop=True)
+    final_training_set = pd.concat([actives, inactives, gen_training]).reset_index(drop=True)
+    print('Initial Training set',final_training_set['Activity'].value_counts())
 
-'''
-5. Combine actives, inactives and RNN/DEG, and remove duplicates with external test set
-'''
+    ext_test_set = pd.read_csv('../Data/Test/M1_test_scaffold_split.csv', index_col=None)
+    PandasTools.AddMoleculeColumnToFrame(frame=ext_test_set, smilesCol='SMILES', molCol='Molecule')
+    ext_test_set['Morgan2FP'] = ext_test_set['Molecule'].map(computeMorganFP)
 
-final_training_set = pd.concat([ac, inac, gen_training]).reset_index(drop=True)
-print('Initial Training set',final_training_set['Activity'].value_counts())
+    source=[]
+    for i in range(final_training_set.shape[0]):
+        for j in range(ext_test_set.shape[0]):
+            if np.array_equal(final_training_set.Morgan2FP[i],ext_test_set.Morgan2FP[j]):
+                print('Public overlap',i,j)
+                source.append(i)
 
-ext_test_set = pd.read_csv('../Data/Test/M1_test_scaffold_split.csv', index_col=None)
-PandasTools.AddMoleculeColumnToFrame(frame=ext_test_set, smilesCol='SMILES', molCol='Molecule')
-ext_test_set['Morgan2FP'] = ext_test_set['Molecule'].map(computeMorganFP)
+    final_training_set = final_training_set.drop(source).reset_index(drop=True)
+    print('Final training set',final_training_set['Activity'].value_counts())
+    print('Ext Test set',ext_test_set['Activity'].value_counts())
 
-source=[]
-for i in range(final_training_set.shape[0]):
-    for j in range(ext_test_set.shape[0]):
-        if np.array_equal(final_training_set.Morgan2FP[i],ext_test_set.Morgan2FP[j]):
-            print('Public',i,j)
-            source.append(i)
+    final_training_set[['SMILES','Activity']].to_csv('../Data/Training/M1/M1_training_set_RNN.csv', index=False)
 
-final_training_set = final_training_set.drop(source).reset_index(drop=True)
-print('Final training set',final_training_set['Activity'].value_counts())
-print('Ext Test set',ext_test_set['Activity'].value_counts())
-
-final_training_set[['SMILES','Activity']].to_csv('../Data/Training/M1_training_set_RNN.csv', index=False)
+if __name__ == "__main__":
+    main()
--- Scripts/pred_sklearn.py
+++ Scripts/pred_sklearn.py
@@ -1,78 +1,76 @@
 import pickle
-import numpy as np
 import pandas as pd
-from rdkit import DataStructs
-from rdkit.Chem import AllChem, PandasTools
-from sklearn.metrics import confusion_matrix,roc_auc_score,matthews_corrcoef
+from rdkit.Chem import PandasTools
 from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import BernoulliNB
-import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix,accuracy_score,roc_auc_score,matthews_corrcoef
 
-'''
-Choose the ML model out of Naive Bayes, Random forest and XGBoost
-'''
+import logging
+import argparse
+from typing import Tuple
 
-clf = BernoulliNB()
-#clf = RandomForestClassifier()
-#lf = xgb.XGBClassifier()
+from utils import computeMorganFP
 
-model = 'nb'
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-'''
-Calculate 1024-bit Morgan fingerprint
-'''
+def get_classifier(model_name: str):
+    if model_name == 'nb':
+        return BernoulliNB()
+    elif model_name == 'rf':
+        return RandomForestClassifier(random_state=42)
+    elif model_name == 'xg':
+        return XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+    else:
+        raise ValueError(f"Unsupported model type: {model_name}")
+    
+def evaluate_model(model, X_test: np.ndarray, y_test: np.ndarray) -> Tuple[float, float, float, float, float, float]:
+    """Evaluate a classification model and return accuracy and ROC AUC."""
+    y_pred = model.predict(list(X_test))
+    acc = accuracy_score(y_test, y_pred)
 
-def computeMorganFP(mol, depth=2, nBits=1024):
-    a = np.zeros(nBits)
-    try:
-      DataStructs.ConvertToNumpyArray(AllChem.GetMorganFingerprintAsBitVect(mol,depth,nBits),a)
-    except:
-      return None
-    return a
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred, labels=[0,1]).ravel()
+    sen = tp/(tp + fn)
+    spec = tn/(tn + fp)
+    g_mean = np.sqrt(sen*spec)
 
-'''
-Load the training set
-'''
-
-training_set = pd.read_csv('../Data/Training/M1_training_set.csv', index_col=None)
-PandasTools.AddMoleculeColumnToFrame(frame=training_set, smilesCol='SMILES', molCol='Molecule')
-training_set['Morgan2FP'] = training_set['Molecule'].map(computeMorganFP)
-X_train_ext = training_set.Morgan2FP
-y_train_ext = training_set.Activity
-
-'''
-Model building and calculating 10-fold stratified cross-validation results
-'''
-
-folds = []
-validation = pd.DataFrame(columns=['Sentitivity','Specificity','MCC','ROC-AUC','G-Mean'])
-
-skf = StratifiedKFold(n_splits=10, shuffle = True, random_state=125)
-labels = [0,1]
-
-for i, (train_index, test_index) in enumerate(skf.split(X_train_ext, y_train_ext)):
-    folds.append({'train':train_index,'test':test_index})
-
-for i in range (len(folds)):
-    X_train = pd.DataFrame(X_train_ext).iloc[folds[i]['train']].to_numpy().ravel()
-    X_test = pd.DataFrame(X_train_ext).iloc[folds[i]['test']].to_numpy().ravel()
-    y_train = pd.DataFrame(y_train_ext).iloc[folds[i]['train']].to_numpy().ravel()
-    y_test = pd.DataFrame(y_train_ext).iloc[folds[i]['test']].to_numpy().ravel()
-
-    clf.fit(list(X_train), y_train)
-    y_pred = clf.predict(list(X_test))
-    tn, fp, fn, tp = confusion_matrix(y_test, y_pred, labels=labels).ravel()
-
-    sensitivity = tp/(tp + fn)
-    specificity = tn/(tn + fp)
+    roc = roc_auc_score(y_test, model.predict_proba(X_test)[:, 1])
     mcc = matthews_corrcoef(y_test, y_pred)
-    roc_auc = roc_auc_score(y_test, y_pred)
-    g_mean = np.sqrt(sensitivity*specificity)
 
-    validation.loc[i,:] = [sensitivity,specificity,mcc,roc_auc,g_mean]
+    logging.info(f"Evaluation -> Accuracy: {acc:.2f}, Sensitivity: {sen:.2f}, Specificity: {spec:.2f}, MCC: {mcc:.2f}, ROC AUC: {roc:.2f}, G-Mean: {g_mean:.2f}")
+    return acc, sen, spec, mcc, roc, g_mean
 
-validation.to_csv('../Data/Results/{}_validation.csv'.format(model))
+def main(model: str):
+    clf = get_classifier(model)
 
-with open('../Data/Models/{}.pkl'.format(model),'wb') as f:
-    pickle.dump(clf,f)
+    training_set = pd.read_csv('../Data/Training/M1/M1_training_set.csv', index_col=None)
+    PandasTools.AddMoleculeColumnToFrame(frame=training_set, smilesCol='SMILES', molCol='Molecule')
+    training_set['Morgan2FP'] = training_set['Molecule'].map(computeMorganFP)
+
+    X = training_set['Morgan2FP'].to_list()
+    y = training_set['Activity'].to_numpy()
+
+    skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=125)
+    validation = pd.DataFrame(columns=['Accuracy', 'Sensitivity', 'Specificity', 'MCC', 'ROC-AUC', 'G-Mean'])
+
+    for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+        X_train = [X[i] for i in train_index]
+        X_test = [X[i] for i in test_index]
+        y_train = y[train_index]
+        y_test = y[test_index]
+
+        clf.fit(X_train, y_train)
+        validation.loc[i] = evaluate_model(clf, X_test, y_test)
+
+    validation.to_csv('../Data/Results/{}_validation.csv'.format(model))
+    with open(f"../Data/Model/{model}.pkl", "wb") as f:
+          pickle.dump(clf, f)
+
+    logging.info(f"Model '{model}' trained and saved. Validation results saved as well.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train and evaluate a model with 10-fold CV on M1 dataset.")
+    parser.add_argument("--model", required=True, choices=["nb", "rf", "xgb"], help="Model type: nb (Naive Bayes), rf (Random Forest), xgb (XGBoost)")
+    args = parser.parse_args()
+    main(args.model)
